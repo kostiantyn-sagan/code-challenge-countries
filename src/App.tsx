986da1988dbec238cc70fcd9a309e06078cc8e30
kostiantyn-sagan <kstelephone8@gmail.com>
2022-05-02 17:29:30 +0300

    Add template

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { Container } from "@mui/material";
+
+function App() {
+  return (
+    <div>
+      <header>
+        <Container>
+          <p>
+            Edit <code>src/App.tsx</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </Container>
+      </header>
+    </div>
+  );
+}
+
+export default App;
